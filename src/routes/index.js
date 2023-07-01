c092ec514d1feb37bfce4886bc07e4c0064d4556
hieuhieuhieu001 <nguyenvantrungjieu@gmail.com>
2023-07-01 21:47:14 +0700

    category

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,9 @@ import Home from '~/pages/Home';
 import Cart from '~/pages/Cart';
 import Produce from '~/pages/Produce';
 import Profile from '~/pages/Profile';
+import Category from '~/pages/category';
 const publishRoutes = [
-    { path: '/', component: Home },
+    { path: '/', component: Category },
     { path: '/cart', component: Cart },
     { path: '/produce', component: Produce },
     { path: '/profile', component: Profile },
